8d3c2e0a28f0e7144f80090d4c670e05413b7b52
Alex Solo <89515014507@mail.ru>
2025-03-26 11:00:57 +0600

    Task end

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,10 +12,39 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+//        int num = -2;
+
+        if(num == 0 || (num < 10 && num > 0) || (num > -10 && num < 0)) {
+            System.out.println("Число не палиндром");
+            return;
+        }
+
+        int count = 0;
+        int numTmp = num;
+
+        while (numTmp != 0){
+            numTmp = numTmp / 10;
+            count++;
+        }
+
+        int[] arrayNums = new int[count];
+        for (int i = 0; i < arrayNums.length; i++) {
+            arrayNums[i] = num % 10;
+            num = num / 10;
+        }
+        for (int i = 0; i < arrayNums.length; i++) {
+            if(arrayNums[i] != arrayNums[arrayNums.length -1 - i ]){
+                System.out.println("Число не палиндром");
+                return;
+            }
+        }
+
+        System.out.println("Число палиндром");
 
 
-        System.out.println(-95 / 10 / 10);
-        System.out.println(-95 % 10);
+
     }
 
 
